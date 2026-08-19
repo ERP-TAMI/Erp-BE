b41a112c4850a8c07cf576c993b8153d89d3c0c6
Trần Kim Thắng <111619268+KeyT9999@users.noreply.github.com>
2026-08-20 06:37:21 +0700

    feat(db): map material group display order

--- src/features/master-data/entities/MaterialGroup.entity.ts
+++ src/features/master-data/entities/MaterialGroup.entity.ts
@@ -12,6 +12,9 @@ export class MaterialGroup {
   @Column({ type: 'varchar', length: 150 })
   name: string;
 
+  @Column({ type: 'integer', name: 'display_order', default: 0 })
+  displayOrder: number;
+
   @Column({ type: 'enum', enum: RecordStatus, enumName: 'record_status' })
   status: RecordStatus;
 }
